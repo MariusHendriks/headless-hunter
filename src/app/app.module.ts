248d27f62296c02e3c0c594656115c4e2d3dc12c
Mark Hendriks <mark.hendriks9999@gmail.com>
2022-02-10 11:43:56 +0100

    Initialised lots of files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BandComponent } from './pages/band/band.component';
+import { ShowsComponent } from './pages/shows/shows.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    BandComponent,
+    ShowsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
