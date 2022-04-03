d24ca188b89489ee07650df7fb303b57ec0be1ae
Mark Hendriks <mark.hendriks9999@gmail.com>
2022-04-03 14:19:30 +0200

    Update shows.component.ts

--- src/app/pages/shows/shows.component.ts
+++ src/app/pages/shows/shows.component.ts
@@ -27,6 +27,15 @@ export class ShowsComponent implements OnInit {
       free: false
     },
     {
+      event: 'Welschap DxIxY Fest',
+      venue: 'DIY',
+      place: 'Eindhoven',
+      country: 'Netherlands',
+      date: '07-05-2022',
+      linkToTickets: 'https://www.facebook.com/events/658978278517772/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A397343618881768%7D%7D]%22%7D',
+      free: false
+    },
+    {
       event: 'Changing Tides + Headless Hunter',
       venue: 'Cafe Govers',
       place: 'Geffen',
@@ -35,17 +44,7 @@ export class ShowsComponent implements OnInit {
       linkToTickets: 'https://www.facebook.com/events/248227777237431',
       free: true
     },
-    {
-      event: 'Welschap DxIxY Fest',
-      venue: 'DIY',
-      place: 'Eindhoven',
-      country: 'Netherlands',
-      date: '07-05-2022',
-      linkToTickets: 'https://www.facebook.com/events/658978278517772/?acontext=%7B%22ref%22%3A%2252%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22share_link%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22share_link%5C%22%2C%5C%22extra_data%5C%22%3A%7B%5C%22invite_link_id%5C%22%3A397343618881768%7D%7D]%22%7D',
-      free: true
-    },
-  ].sort((val1, val2)=> { // @ts-ignore
-    return new Date(val1.date) - new Date(val2.date)})
+  ]
   constructor() { }
 
   ngOnInit(): void {
